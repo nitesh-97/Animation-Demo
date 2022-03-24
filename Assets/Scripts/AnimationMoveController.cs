9594d7324fe0d6a7937520b66008ecbf4c0066e4
Nitesh Kumar Kabi <niteshkabi1997@gmail.com>
2022-03-25 02:44:35 +0530

    Initial commit

---
+++ Assets/Scripts/AnimationMoveController.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class AnimationMoveController : MonoBehaviour
+{
+    PlayerInput playerInput;
+    CharacterController characterController;
+    Animator animator;
+
+    Vector2 CurrentMovementInput;
+    Vector3 currentMovement;
+    bool ismovementpressed;
+    float rotationFactorPerFrame = 2f;
+
+    private void Awake()
+    {
+        playerInput = new PlayerInput();
+        animator = GetComponent<Animator>();
+        characterController = GetComponent<CharacterController>();
+        playerInput.CharacterControls.Move.started += OnMovementInput;
+        playerInput.CharacterControls.Move.canceled += OnMovementInput;
+        playerInput.CharacterControls.Move.performed += OnMovementInput;
+    }
+    void OnMovementInput(InputAction.CallbackContext context)
+    {
+        CurrentMovementInput = context.ReadValue<Vector2>();
+        currentMovement.x = CurrentMovementInput.x;
+        currentMovement.z = CurrentMovementInput.y;
+        ismovementpressed = (CurrentMovementInput.x != 0 || CurrentMovementInput.y != 0);
+    }
+    void HandleAnimation()
+    {
+        bool IsWalking = animator.GetBool("IsWalking");
+        if (ismovementpressed && !IsWalking)
+        {
+           // animator.SetBool("Isidle", false);
+            animator.SetBool("IsWalking", true);
+        }
+        else if(!ismovementpressed && IsWalking)
+        {
+            //animator.SetBool("Isidle", true);
+            animator.SetBool("IsWalking", false);
+        }
+    }
+    void HandleGravity()
+    {
+        if (characterController.isGrounded)
+        {
+            float groundedGravity = -.85f;
+            currentMovement.y = groundedGravity;
+        }
+        else
+        {
+            float gravity = -9.8f;
+            currentMovement.y += gravity;
+        }
+    }
+    void HandleRotation()
+    {
+        Vector3 positiontolookat;
+        positiontolookat.x = currentMovement.x;
+        positiontolookat.y = 0.0f;
+        positiontolookat.z = currentMovement.z;
+        Quaternion currentRotation = transform.rotation ;
+        if (ismovementpressed)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(positiontolookat);
+           transform.rotation = Quaternion.Slerp(currentRotation, targetRotation, rotationFactorPerFrame*Time.deltaTime);
+        }
+        
+    }
+
+   
+    private void OnEnable()
+    {
+        playerInput.CharacterControls.Enable();
+    }
+    private void OnDisable()
+    {
+        playerInput.CharacterControls.Disable();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        HandleGravity();
+        HandleRotation();
+        HandleAnimation();
+        
+        characterController.Move(currentMovement*Time.deltaTime); 
+        
+    }
+}
---
+++ Assets/Scripts/Player Input.cs
@@ -0,0 +1,187 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Animations/Player Input.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @PlayerInput : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @PlayerInput()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""Player Input"",
+    ""maps"": [
+        {
+            ""name"": ""Character Controls"",
+            ""id"": ""6418bfbc-0a4b-4c62-bd97-537fd2a51819"",
+            ""actions"": [
+                {
+                    ""name"": ""Move"",
+                    ""type"": ""Value"",
+                    ""id"": ""31cbc76f-96b2-4d0e-996b-c7679cc3e141"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": ""NormalizeVector2"",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""e6bcf28d-2ff8-420e-8a6a-7fecbef7076f"",
+                    ""path"": """",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""2D Vector"",
+                    ""id"": ""1a680056-8bfd-4766-ba09-4e21175059e9"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""2e9610b3-10da-4051-bfee-7629c1b3153d"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""77876aa5-0761-4d59-b5c1-343fce33ad9b"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""91a6e0da-78cd-4407-ab3e-70660d9f0311"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""da9bb456-015b-41cd-909d-5d18f49810bd"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Character Controls
+        m_CharacterControls = asset.FindActionMap("Character Controls", throwIfNotFound: true);
+        m_CharacterControls_Move = m_CharacterControls.FindAction("Move", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Character Controls
+    private readonly InputActionMap m_CharacterControls;
+    private ICharacterControlsActions m_CharacterControlsActionsCallbackInterface;
+    private readonly InputAction m_CharacterControls_Move;
+    public struct CharacterControlsActions
+    {
+        private @PlayerInput m_Wrapper;
+        public CharacterControlsActions(@PlayerInput wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Move => m_Wrapper.m_CharacterControls_Move;
+        public InputActionMap Get() { return m_Wrapper.m_CharacterControls; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(CharacterControlsActions set) { return set.Get(); }
+        public void SetCallbacks(ICharacterControlsActions instance)
+        {
+            if (m_Wrapper.m_CharacterControlsActionsCallbackInterface != null)
+            {
+                @Move.started -= m_Wrapper.m_CharacterControlsActionsCallbackInterface.OnMove;
+                @Move.performed -= m_Wrapper.m_CharacterControlsActionsCallbackInterface.OnMove;
+                @Move.canceled -= m_Wrapper.m_CharacterControlsActionsCallbackInterface.OnMove;
+            }
+            m_Wrapper.m_CharacterControlsActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Move.started += instance.OnMove;
+                @Move.performed += instance.OnMove;
+                @Move.canceled += instance.OnMove;
+            }
+        }
+    }
+    public CharacterControlsActions @CharacterControls => new CharacterControlsActions(this);
+    public interface ICharacterControlsActions
+    {
+        void OnMove(InputAction.CallbackContext context);
+    }
+}
